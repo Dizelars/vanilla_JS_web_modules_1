218757daa7c67417ddc83a25662072dfeae62304
Dizelars <ars_logachev@mail.ru>
2023-05-03 10:58:51 +0300

    tabs js-code add

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -7,11 +7,32 @@ const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
         content.forEach(item => {
             item.style.display = "none";
         });
+
+        tab.forEach(item => {
+            item.classList.remove(activeClass);
+        });
     }
 
-    function showTabContent(i) { // Работает только если у нас блочные элементы, не флексы. (Для флексов можно поставить проверку, является ли данный блок флексом или блоком)
-        content[i].style.display = "block"
+    function showTabContent(i = 0) { // Работает только если у нас блочные элементы, не флексы. (Для флексов можно поставить проверку, является ли данный блок флексом или блоком)
+        content[i].style.display = "block";
+        tab[i].classList.add(activeClass);
     }
+
+    hideTabContent();
+    showTabContent();
+
+    // Применяем делегирование событий, (Вешаем обработчик на весь блок, и проверяем, на какой элемент кликнул пользователь)ю
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target && (target.classList.contains(tabSelector.replace(/\./, "")) || target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {
+            tab.forEach((item, i) => {
+                if (target === item || target.parentNode === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
 };
 
 export default tabs;
