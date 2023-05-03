7e2f37a88d9c835689e47caf3704229a5d13e472
Dizelars <ars_logachev@mail.ru>
2023-05-03 11:37:26 +0300

    add cmments to tabs.js

--- dist/script.js
+++ dist/script.js
@@ -52,12 +52,14 @@ __webpack_require__.r(__webpack_exports__);
 const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
   const header = document.querySelector(headerSelector),
     tab = document.querySelectorAll(tabSelector),
-    content = document.querySelectorAll(contentSelector);
+    content = document.querySelectorAll(contentSelector); // Берем элементы, которые мы передаем внутрь функции tabs() при ее вызове.
+
   function hideTabContent() {
     content.forEach(item => {
       item.style.display = "none";
     });
     tab.forEach(item => {
+      // Для каждого элемента tab удаляем класс активности(тот что мы передаем последним аргументом в функцию).
       item.classList.remove(activeClass);
     });
   }
@@ -65,24 +67,34 @@ const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
     let i = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
     // Работает только если у нас блочные элементы, не флексы. (Для флексов можно поставить проверку, является ли данный блок флексом или блоком)
     content[i].style.display = "block";
-    tab[i].classList.add(activeClass);
+    tab[i].classList.add(activeClass); // Добавляем класс активности к контенту, который нужно показать(нам приходит его индекс, именно тот, по которому кликнул пользователь).
   }
-  hideTabContent();
-  showTabContent();
 
-  // Применяем делегирование событий, (Вешаем обработчик на весь блок, и проверяем, на какой элемент кликнул пользователь)ю
+  hideTabContent(); // Вызываем функцию скрытия элементов
+  showTabContent(); // Вызываем функцию показа элементов
+
+  // Применяем делегирование событий (Вешаем обработчик на весь блок, и проверяем, на какой элемент кликнул пользователь).
   header.addEventListener('click', e => {
+    // Вешаем обработчик события на блок со всеми элементами, на которые будем кликать
     const target = e.target;
     if (target && (target.classList.contains(tabSelector.replace(/\./, "")) || target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {
+      // Делаем несколько проверок:
+      // 1. Существует ли вообще событие клик на элемент
+      // 2. Содержит ли элемент класс, передаваемый во 2-ой аргумент без точки класса(для этого и нужны регулярные выражения)
+      // 3. Или содержит ли родительский элемент, элемента на который кликнули, класс передаваемый во 2-ой аргумент без точки класса(для этого и нужны регулярные выражения)
       tab.forEach((item, i) => {
+        // Если все проверки прошли, то оставшиеся элементы в переменной tab перебираются, и если элемент по которому прошел клик, равеен элементу или его родителю в массиве tab, то:
         if (target === item || target.parentNode === item) {
           hideTabContent();
           showTabContent(i);
+          // 1) Скрываем каждый полученный элемент после проверки.
+          // 2) Показываем именно тот контент, который соответствует индексу элемента в массиве tab.
         }
       });
     }
   });
 };
+
 /* harmony default export */ __webpack_exports__["default"] = (tabs);
 
 /***/ }),
@@ -14255,6 +14267,7 @@ __webpack_require__.r(__webpack_exports__);
 window.addEventListener('DOMContentLoaded', () => {
   (0,_modules_modals__WEBPACK_IMPORTED_MODULE_1__["default"])();
   (0,_modules_tabs__WEBPACK_IMPORTED_MODULE_2__["default"])('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+  (0,_modules_tabs__WEBPACK_IMPORTED_MODULE_2__["default"])('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 });
 }();
 /******/ })()
--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,34 +1,41 @@
 const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
     const header = document.querySelector(headerSelector),
           tab = document.querySelectorAll(tabSelector),
-          content = document.querySelectorAll(contentSelector);
+          content = document.querySelectorAll(contentSelector); // Берем элементы, которые мы передаем внутрь функции tabs() при ее вызове.
 
     function hideTabContent() {
         content.forEach(item => {
             item.style.display = "none";
         });
 
-        tab.forEach(item => {
+        tab.forEach(item => { // Для каждого элемента tab удаляем класс активности(тот что мы передаем последним аргументом в функцию).
             item.classList.remove(activeClass);
         });
     }
 
     function showTabContent(i = 0) { // Работает только если у нас блочные элементы, не флексы. (Для флексов можно поставить проверку, является ли данный блок флексом или блоком)
         content[i].style.display = "block";
-        tab[i].classList.add(activeClass);
+        tab[i].classList.add(activeClass); // Добавляем класс активности к контенту, который нужно показать(нам приходит его индекс, именно тот, по которому кликнул пользователь).
     }
 
-    hideTabContent();
-    showTabContent();
+    hideTabContent(); // Вызываем функцию скрытия элементов
+    showTabContent(); // Вызываем функцию показа элементов
 
-    // Применяем делегирование событий, (Вешаем обработчик на весь блок, и проверяем, на какой элемент кликнул пользователь)ю
-    header.addEventListener('click', (e) => {
+    // Применяем делегирование событий (Вешаем обработчик на весь блок, и проверяем, на какой элемент кликнул пользователь).
+    header.addEventListener('click', (e) => { // Вешаем обработчик события на блок со всеми элементами, на которые будем кликать
         const target = e.target;
         if (target && (target.classList.contains(tabSelector.replace(/\./, "")) || target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {
+// Делаем несколько проверок:
+// 1. Существует ли вообще событие клик на элемент
+// 2. Содержит ли элемент класс, передаваемый во 2-ой аргумент без точки класса(для этого и нужны регулярные выражения)
+// 3. Или содержит ли родительский элемент, элемента на который кликнули, класс передаваемый во 2-ой аргумент без точки класса(для этого и нужны регулярные выражения)
             tab.forEach((item, i) => {
+// Если все проверки прошли, то оставшиеся элементы в переменной tab перебираются, и если элемент по которому прошел клик, равеен элементу или его родителю в массиве tab, то:
                 if (target === item || target.parentNode === item) {
                     hideTabContent();
                     showTabContent(i);
+                    // 1) Скрываем каждый полученный элемент после проверки.
+                    // 2) Показываем именно тот контент, который соответствует индексу элемента в массиве tab.
                 }
             });
         }
